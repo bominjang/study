b58c878668c6bc9a228de516a2e9fda25a46c155
bominjang <qhdud9443@naver.com>
2020-01-20 16:40:40 +0900

    solve NPE by adding @SpringBootTest annotation

--- src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
+++ src/test/java/com/example/study/repository/OrderDetailRepositoryTest.java
@@ -5,10 +5,12 @@ import com.example.study.model.entity.OrderDetail;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
-public class OrderDetailRepositoryTest extends StudyApplication {
+@SpringBootTest
+public class OrderDetailRepositoryTest{
     @Autowired
     private OrderDetailRepository orderDetailRepository;
 
