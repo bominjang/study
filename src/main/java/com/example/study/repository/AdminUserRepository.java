fcb54b9b380249a0ef86778e7ec8209c05d0d300
bominjang <qhdud9443@naver.com>
2020-01-22 23:16:17 +0900

    Create Repository

---
+++ src/main/java/com/example/study/repository/AdminUserRepository.java
@@ -0,0 +1,10 @@
+package com.example.study.repository;
+
+import com.example.study.model.entity.AdminUser;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminUserRepository extends JpaRepository<AdminUser, Long> {
+}
