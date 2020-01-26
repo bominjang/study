191237e81e44ed4895fe1ea4ab7ed9e6ad675bc7
bominjang <qhdud9443@naver.com>
2020-01-26 23:41:28 +0900

    Create JpaCofig, LoginUserAuditorAware

--- src/main/java/com/example/study/model/entity/AdminUser.java
+++ src/main/java/com/example/study/model/entity/AdminUser.java
@@ -4,17 +4,21 @@ import com.sun.xml.internal.ws.api.ha.StickyFeature;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 @Entity
+//해당 entityListener는 이 감시자 EntityListener를 사용하겠다!
+@EntityListeners(AuditingEntityListener.class)
 public class AdminUser {
 
     @Id
@@ -39,12 +43,16 @@ public class AdminUser {
 
     private LocalDateTime unregisteredAt;
 
+    @CreatedDate
     private LocalDateTime createdAt;
 
+    @CreatedBy
     private String createdBy;
 
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     private String updatedBy;
 
 }
