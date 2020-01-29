62bad7958dc081f93f2994bbb16486286ae12d8e
bominjang <qhdud9443@naver.com>
2020-01-29 09:00:03 +0900

    UserApi ServiceLogic 작성

--- src/main/java/com/example/study/service/UserApiLogicService.java
+++ src/main/java/com/example/study/service/UserApiLogicService.java
@@ -1,12 +1,15 @@
 package com.example.study.service;
 
 import com.example.study.ifs.CrudInterface;
+import com.example.study.model.entity.User;
 import com.example.study.model.network.Header;
 import com.example.study.model.network.request.UserApiRequest;
 import com.example.study.model.network.response.UserApiResponse;
 import com.example.study.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 //해당 java project가 service로 동작할 수 있도록!
 @Service
@@ -21,7 +24,24 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     //3. 생성된 데이터 -> UserApiResponse return
     @Override
     public Header<UserApiResponse> create(Header<UserApiRequest> request) {
+        //1.reaueset data
+        UserApiRequest userApiRequest = request.getData();
 
+        //2. User 생성 - client 단에서 받은 정보로 User 객체 생성
+        User user = User.builder()
+                .account(userApiRequest.getAccount())
+                .password(userApiRequest.getPassword())
+                .status("REGISTERED")
+                .phoneNumber(userApiRequest.getPhoneNumber())
+                .email(userApiRequest.getEmail())
+                .registeredAt(LocalDateTime.now())
+                .build();
+        User newUser = userRepository.save(user);
+
+        //3. 생성된 데이터 -> userApiResponse return
+        //이것은 read나 update 등 다른 메소드에서도 사용할 수 있으니,따로 메소드로 빼자!
+
+        return response(newUser);
 
 
         return null;
@@ -40,5 +60,22 @@ public class UserApiLogicService implements CrudInterface<UserApiRequest, UserAp
     @Override
     public Header<UserApiResponse> delete(Long id) {
         return null;
+    }
+
+    private Header<UserApiResponse> response(User user){
+        //user 객체를 받아서 userApiResponse를 만들어서 내보내는 메소드!
+        UserApiResponse userApiResponse = UserApiResponse.builder()
+                .id(user.getId())
+                .account(user.getAccount())
+                .password(user.getPassword()) // todo 암호화, 길이
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .status(user.getStatus())
+                .registeredAt(user.getRegisteredAt())
+                .unregisteredAt(user.getUnregisteredAt())
+                .build();
+
+        //Header + data return
+        return Header.OK(userApiResponse);
     }
 }
